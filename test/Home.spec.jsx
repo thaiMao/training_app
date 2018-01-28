ffe0c2ea67585581067722c45e8464b09f9613f8
Mao <maot.tm@gmail.com>
2018-01-28 21:03:08 +0000

    Added enzyme and basic tests for home page

---
+++ test/Home.spec.jsx
@@ -0,0 +1,19 @@
+/* eslint-env  mocha */
+
+const { expect } = require("chai");
+const React = require("react");
+const Home = require("../src/Home");
+const Button = require("../src/Components/Button");
+const { shallow } = require("enzyme");
+
+describe("<Home /> ", () => {
+  it("Should render a login button", () => {
+    const wrapped = shallow(<Home />);
+    expect(wrapped.contains(<Button>Login</Button>));
+  });
+
+  xit("Should render the brand", () => {
+    const wrapped = shallow(<Home />);
+    expect(wrapped.contains(<h1>My Brand</h1>)).to.be.true;
+  });
+});
