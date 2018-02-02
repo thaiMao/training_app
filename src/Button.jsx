5a30709eb45394f7ec5bfb864af98dacc111cf1a
Mao <maot.tm@gmail.com>
2018-02-02 21:10:42 +0000

    Added Button Component

---
+++ src/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const Button = props => {
+  return <StyledButton {...props} />;
+};
+
+const StyledButton = styled.button``;
+
+export default Button;
